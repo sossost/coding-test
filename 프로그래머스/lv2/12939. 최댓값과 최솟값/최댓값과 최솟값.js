17682cf8c2d2406d603afbe01358b2f76dc5ba95
yoonsu <110542210+sossost@users.noreply.github.com>
2023-07-24 14:09:42 +0900

    Update 최댓값과 최솟값.js

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -2,3 +2,5 @@ function solution(s) {
     const arr = s.split(' ')
     return arr.sort((a,b)=>a-b).filter((_,i)=> i===0 || i===arr.length-1 ).join(' ');
 }
+
+// Math.min() / Math.max() 메소드 활용하기 배열에서 최솟값,최댓값을 리턴함
