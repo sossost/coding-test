12d73c230f4a38ffa9c7e95481d07d86b19ea5dc
yoonsu <110542210+sossost@users.noreply.github.com>
2023-07-24 13:17:40 +0900

    Update 가운데 글자 가져오기.js

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -3,3 +3,5 @@ function solution(s) {
     const centerIndex = ~~(s.length/2)
     return [...s].filter((x,i)=>length%2 ? i === centerIndex : i === centerIndex-1|| i=== centerIndex).join('')
 }
+
+// string.subString 이라는 문자열 자르는 메소드가 있음
