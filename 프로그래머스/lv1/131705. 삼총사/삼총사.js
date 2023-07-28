a497cca5116a182d0a00d74fd3cf00b2264297c7
yoonsu <110542210+sossost@users.noreply.github.com>
2023-07-28 17:58:08 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,9 +1,9 @@
 function solution(number) {
     let result = 0
     for(let i = 0 ; i<number.length; i++){
-        for(let j = i; j<number.length ; j++){
-            for(let k = j; k<number.length ; k++){
-                if(i !== j && j !==k && i !== k && number[i]+number[j]+number[k] === 0){
+        for(let j = i+1; j<number.length ; j++){
+            for(let k = j+1; k<number.length ; k++){
+                if(number[i]+number[j]+number[k] === 0){
                      result += 1
                 }
             }
