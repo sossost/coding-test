68bd68a29bdb511ce312f5d0855d19143042d1c8
윤수 <110542210+sossost@users.noreply.github.com>
2023-08-10 14:28:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    if (n === 1) {
+        return 1;
+    }
+    if (n === 2) {
+        return 2;
+    }
+
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+
+    return dp[n];
+}
